25858a336c939f14d9676bcbdf4866496cbc2957
Basile Quinchon <basile.quinchon@gmail.com>
2020-04-21 18:40:30 +0200

    navbar & presentation 1

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,18 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap";
-import { scrolling } from '../plugins/scrolling';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { navToggle } from '../plugins/nav_toggle';
+import { initRellax } from "plugins/rellax.js";
 
-scrolling();
+// NAVBAR ANIMATION
+navToggle();
+
+//RELLAX INIT
+if (document.querySelector('.rellax')) {
+  initRellax();
+};
+
+// ANIMATE ON SCROLL INIT
+AOS.init();
--- app/javascript/plugins/scrolling.js
+++
@@ -1,9 +0,0 @@
-const scrolling = () => {
-
-  const elements = document.querySelectorAll('.anchor')
-  document.addEventListener('wheel', (event) => {
-    // TODO : Scroll to next element with the anchor class in the view
-  })
-};
-
-export { scrolling };

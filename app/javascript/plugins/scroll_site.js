db064b2d785ee990863948ba07b6292c1c476925
Basile Quinchon <basile.quinchon@gmail.com>
2020-04-25 21:06:46 +0200

    better scrolling

--- app/javascript/plugins/scroll_site.js
+++ app/javascript/plugins/scroll_site.js
@@ -4,8 +4,8 @@ const scrollSite = () => {
   window.addEventListener('scroll', (event) => {
     if (window.scrollY > 2103) {
       const pourcentage = (window.scrollY - 2103) * 100 / 800
-      phone.style.backgroundPosition = `center ${pourcentage}%`;
-      site.style.backgroundPosition = `center ${pourcentage}%`;
+      phone.style.backgroundPosition = `center ${(pourcentage / 6) - 20}%`;
+      site.style.backgroundPosition = `center ${(pourcentage / 2) - 30}%`;
     }
   })
 }

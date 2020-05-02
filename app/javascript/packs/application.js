fe676e67c8f45498b72e0902a8503677602c7205
Basile Quinchon <basile.quinchon@gmail.com>
2020-05-02 22:12:05 +0200

    correct import assets

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,5 @@
 require("@rails/ujs").start()
+require("turbolinks").start()
 require("@rails/activestorage").start()
 
 import $ from 'jquery';
@@ -9,26 +10,28 @@ import { navToggle } from '../plugins/nav_toggle';
 import { scrollSite } from '../plugins/scroll_site';
 import { initRellax } from "plugins/rellax.js";
 
-// JQUERY GLOBAL $ FOR AJAX
-global.$ = jQuery;
-// WELCOME MESSAGE
-console.log('%c%s',
-            "color: #FFF; background: #f2a365; font-size: 24px; padding: .5rem; font-family: 'courier'; text-align: center;",
-            'Greetings !');
+document.addEventListener('turbolinks:load', () => {
+  // JQUERY GLOBAL $ FOR AJAX
+  global.$ = jQuery;
+  // WELCOME MESSAGE
+  console.log('%c%s',
+              "color: #FFF; background: #f2a365; font-size: 24px; padding: .5rem; font-family: 'courier'; text-align: center;",
+              'Greetings !');
 
-console.log('%c%s',
-            "color: #FFF; background: #333; font-family: 'courier'; padding: .25rem;",
-            "If you like what you see, feel free to follow me on\nhttps://www.github.com/basilequinchon");
-// NAVBAR ANIMATION
-navToggle();
+  console.log('%c%s',
+              "color: #FFF; background: #333; font-family: 'courier'; padding: .25rem;",
+              "If you like what you see, feel free to follow me on\nhttps://www.github.com/basilequinchon");
+  // NAVBAR ANIMATION
+  navToggle();
 
-// SCROLL ON SITES
-scrollSite();
+  // SCROLL ON SITES
+  scrollSite();
 
-//RELLAX INIT
-if (document.querySelector('.rellax')) {
-  initRellax();
-};
+  //RELLAX INIT
+  if (document.querySelector('.rellax')) {
+    initRellax();
+  };
 
-// ANIMATE ON SCROLL INIT
-AOS.init();
+  // ANIMATE ON SCROLL INIT
+  AOS.init();
+});

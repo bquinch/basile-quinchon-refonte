60cfb68c6d061364d503c1cf151c6ee49081f0f3
Basile Quinchon <basile.quinchon@gmail.com>
2020-05-02 18:20:58 +0200

    adding rails ujs start and side fullstack title

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -15,6 +15,6 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:name, :email, :subject, :body)
+    params.require(:message).permit(:name, :email, :body)
   end
 end

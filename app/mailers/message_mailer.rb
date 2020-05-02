64e16a41dd8b592a3f9f3f580d6529ccf790cc8a
Basile Quinchon <basile.quinchon@gmail.com>
2020-05-02 18:24:45 +0200

    message update (for crash controll, removed subject) and yarn update

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -8,7 +8,7 @@ class MessageMailer < ApplicationMailer
     mg_client = Mailgun::Client.new ENV['MAILGUN_API_KEY']
     message_params = {:from => message.email,
                       :to => 'basile.quinchon@gmail.com',
-                      :subject => message.subject,
+                      :subject => "Nouveau message depuis le site basilequinchon.com",
                       :text => message.body}
     mg_client.send_message 'sandboxf777fcf6c09047d0a1b259b62580524b.mailgun.org', message_params
   end
